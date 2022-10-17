68add614eef795e3e9972f3340d8ad0af6cbe9b3
muntaha rashid <muntaharashid3@gmail.com>
2022-10-17 14:02:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculatorKey from './Components/CalculatorKey';
+
+function App() {
+  return (
+    <div>
+     <CalculatorKey />
+     </div>
+  ) ;
+}
+
+export default App;
